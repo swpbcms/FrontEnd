80dd2264a8f615be0eaf3f2f9add47c49268e4e8
MTT18 <lammjnhphong4560@gmail.com>
2023-06-07 12:06:56 +0700

    update

--- js/login.js
+++ js/login.js
@@ -2,14 +2,17 @@ var memberUserName = document.querySelector('#memberUserName')
 var memberPassword = document.querySelector('#memberPassword')
 var form = document.querySelector('form')
 
-function showError(input, message){
+
+
+
+function showError(input, message) {
     let parent = input.parentElement;
     let small = parent.querySelector('small')
 
     parent.classList.add('error')
     small.innerText = message
 }
-function showSuccess(input){
+function showSuccess(input) {
     let parent = input.parentElement;
     let small = parent.querySelector('small')
 
@@ -17,30 +20,42 @@ function showSuccess(input){
     small.innerText = ''
 }
 
-function checkEmpty(listInput){
-        let isEmptyError = false;
-        listInput.value = listInput.value.trim()
+function checkEmpty(listInput) {
+    let isEmptyError = false;
+    listInput.value = listInput.value.trim()
 
-            if(listInput.value == ''){
-                isEmptyError = true;
-                showError(listInput, 'Khong dc de trong')
-            }else{
-                showSuccess(listInput)
-            }
-        return isEmptyError
+    if (listInput.value == '') {
+        isEmptyError = true;
+        showError(listInput, 'Khong dc de trong')
+    } else {
+        showSuccess(listInput)
+    }
+    return isEmptyError
 }
 
-form.addEventListener('submit', function(e){
+
+form.addEventListener('submit', function (e) {
     e.preventDefault()
 
     let isEmptyError = [memberUserName, memberPassword]
     isEmptyError.forEach(checkEmpty)
+    var xhttp = new XMLHttpRequest();
 
-    if (isEmptyError){
+    if (isEmptyError) {
 
-    }else{
+    } else {
+        xhttp.open('POST', 'https://localhost:7206/api/Member/login-Member', true)
+
+        xhttp.send(memberUserName, memberPassword);
         
+        xhttp.onreadystatechange = function () {
+            if (this.readyState == 4) {
+                window.location.href = 'feed.html'
+                this.responseText;
+            }
+        }
     }
+
 })
 
 
