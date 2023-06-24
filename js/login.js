2fa8c51b331dcb63affe886a43ce8b46e79d7bbd
khai14tv <khai14tv@gmail.com>
2023-06-25 00:59:10 +0700

    admin.html và thêm login admin trong login js

--- js/login.js
+++ js/login.js
@@ -39,7 +39,7 @@
 //     isEmptyError.forEach(checkEmpty)
 
 //     if (isEmptyError) {
-        
+
 //     } else {
 //         fetch(login, {
 //             method: 'POST',
@@ -89,27 +89,27 @@
 $(document).ready(function() {
     // Xử lý sự kiện submit của biểu mẫu
     $("#loginForm").submit(function(event) {
-      event.preventDefault(); // Ngăn chặn gửi yêu cầu mặc định
-  
-      // Lấy giá trị tên đăng nhập và mật khẩu từ người dùng
-      var username = $("#memberUserNameInput").val();
-      var password = $("#memberPasswordInput").val();
-  
-      // Tạo đối tượng chứa dữ liệu gửi đi
-      var data = {
-        memberUserName: username,
-        memberPassword: password
-      };
-  
-      // Gửi yêu cầu AJAX POST đến API
-      $.ajax({
-        url: "https://localhost:7206/api/Member/login-Member",
-        type: "POST",
-        contentType: "application/json",
-        data: JSON.stringify(data),
-        success: function(response) {
-          // Xử lý phản hồi từ API
-          if (response.data) {
+        event.preventDefault(); // Ngăn chặn gửi yêu cầu mặc định
+
+        // Lấy giá trị tên đăng nhập và mật khẩu từ người dùng
+        var username = $("#memberUserNameInput").val();
+        var password = $("#memberPasswordInput").val();
+
+        // Tạo đối tượng chứa dữ liệu gửi đi
+        var data = {
+            memberUserName: username,
+            memberPassword: password
+        };
+
+        // Gửi yêu cầu AJAX POST đến API
+        $.ajax({
+            url: "https://localhost:7206/api/Member/login-Member",
+            type: "POST",
+            contentType: "application/json",
+            data: JSON.stringify(data),
+            success: function(response) {
+                // Xử lý phản hồi từ API
+                /*         if (response.data) {
             // Kiểm tra dữ liệu phản hồi nếu thành công
             var member = response.data;
             // Xử lý dữ liệu thành viên ở đây
@@ -128,6 +128,30 @@ $(document).ready(function() {
       });
     });
   });
-
-
-
+*/
+                if (response.data) {
+                    // Kiểm tra dữ liệu phản hồi nếu thành công
+                    var member = response.data;
+                    if (member.role === "admin") {
+                        // Người dùng có vai trò là admin
+                        // Xử lý dữ liệu ở đây
+                        window.location.href = "admin.html";
+                    } else {
+                        // Người dùng không có vai trò là admin
+                        // Xử lý dữ liệu thành viên ở đây
+                        window.location.href = "index.html";
+                    }
+                    // Lưu session đăng nhập
+                    sessionStorage.setItem("loggedInMember", JSON.stringify(member));
+                } else {
+                    // Xử lý khi đăng nhập không thành công
+                    console.log("Tên đăng nhập hoặc mật khẩu không đúng");
+                }
+            },
+            error: function(error) {
+                // Xử lý khi có lỗi xảy ra trong quá trình gửi yêu cầu
+                console.log("Đã xảy ra lỗi: " + error);
+            }
+        });
+    });
+});
