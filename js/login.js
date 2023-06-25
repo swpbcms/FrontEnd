5e89ad85377725dfa0c76afa0d2e7f01cd505298
nghiant02 <nghia2852002@gmail.com>
2023-06-25 13:42:01 +0700

    change admin.html to manager.html & hanlde login.js to connect API

--- js/login.js
+++ js/login.js
@@ -1,157 +1,69 @@
-// var memberUserName = document.querySelector('#memberUserName')
-// var memberPassword = document.querySelector('#memberPassword')
-// var form = document.querySelector('.c-form')
-
-// function showError(input, message) {
-//     let parent = input.parentElement;
-//     let small = parent.querySelector('small')
-
-//     parent.classList.add('error')
-//     small.innerText = message
-// }
-// function showSuccess(input) {
-//     let parent = input.parentElement;
-//     let small = parent.querySelector('small')
-
-//     parent.classList.remove('error')
-//     small.innerText = ''
-// }
-
-// function checkEmpty(listInput) {
-//     let isEmptyError = false;
-//     listInput.value = listInput.value.trim()
-
-//     if (listInput.value == '') {
-//         isEmptyError = true;
-//         showError(listInput, 'Can not empty')
-//     } else {
-//         showSuccess(listInput)
-//     }
-//     return isEmptyError
-// }
-
-// const login = "https://localhost:7206/api/Member/login-Member";
-
-// form.addEventListener('submit', function (e) {
-//     e.preventDefault()
-
-//     var isEmptyError = [memberUserName, memberPassword]
-//     isEmptyError.forEach(checkEmpty)
-
-//     if (isEmptyError) {
-
-//     } else {
-//         fetch(login, {
-//             method: 'POST',
-//             headers: {
-//                 Accept: "application/json, */*",
-//                 "Content-Type": "application/json"
-//             },
-//             body: JSON.stringify({
-//                 memberUserName: memberUserName.value,
-//                 memberPassword: memberPassword.value
-//             })
-//         })
-//             .then((response) => response.json())
-//             .then((data) => {
-//                 console.log(data);
-//                 if (data.success) { 
-//                     window.open(
-//                         "index.html"
-//                     );
-//                 } else {
-//                     alert("Error UserName or Password");
-//                 }
-//             })
-//             .catch(error => {
-//                 console.error('Lỗi:', error);
-//             });
-//     }
-
-// })
-
-
-// fetch ('https://localhost:7206/swagger/index.html')
-// .then(res => res.json())
-// .then(data => console.log(data))
-
-// const express = require("express")
-// const app = express()
-// const cors = require("cors")
-// app.use(
-//     cors({
-//         origin: "*"
-//     })
-// )
-
-// app.listen(7206)
-
 $(document).ready(function() {
-    // Xử lý sự kiện submit của biểu mẫu
+    // Xử lý sự kiện khi biểu mẫu được gửi đi
     $("#loginForm").submit(function(event) {
-        event.preventDefault(); // Ngăn chặn gửi yêu cầu mặc định
-
-        // Lấy giá trị tên đăng nhập và mật khẩu từ người dùng
-        var username = $("#memberUserNameInput").val();
-        var password = $("#memberPasswordInput").val();
-
-        // Tạo đối tượng chứa dữ liệu gửi đi
-        var data = {
-            memberUserName: username,
-            memberPassword: password
-        };
-
-        // Gửi yêu cầu AJAX POST đến API
-        $.ajax({
-            url: "https://localhost:7206/api/Member/login-Member",
-            type: "POST",
-            contentType: "application/json",
-            data: JSON.stringify(data),
-            success: function(response) {
-                // Xử lý phản hồi từ API
-                /*         if (response.data) {
-            // Kiểm tra dữ liệu phản hồi nếu thành công
+      event.preventDefault(); // Ngăn chặn việc gửi yêu cầu mặc định của biểu mẫu
+  
+      // Lấy tên đăng nhập và mật khẩu từ người dùng
+      var username = $("#userNameInput").val();
+      var password = $("#passwordInput").val();
+  
+      // Tạo đối tượng dữ liệu cho thành viên
+      var memberData = {
+        memberUserName: username,
+        memberPassword: password
+      };
+  
+      // Tạo đối tượng dữ liệu cho quản lý
+      var managerData = {
+        managerUserName: username,
+        managerPassword: password
+      };
+  
+      // Gửi yêu cầu AJAX POST đến API đăng nhập cho thành viên
+      $.ajax({
+        url: "https://localhost:7206/api/Member/login-Member",
+        type: "POST",
+        contentType: "application/json",
+        data: JSON.stringify(memberData),
+        success: function(response) {
+          if (response.data) {
+            // Xử lý đăng nhập thành viên thành công
             var member = response.data;
-            // Xử lý dữ liệu thành viên ở đây
-            // Ví dụ: chuyển trang và lưu session đăng nhập
             window.location.href = "index.html";
             sessionStorage.setItem("loggedInMember", JSON.stringify(member));
           } else {
-            // Xử lý khi đăng nhập không thành công
-            console.log("Tên đăng nhập hoặc mật khẩu không đúng");
+            // Xử lý khi tên đăng nhập hoặc mật khẩu thành viên không đúng
+            console.log("Tên đăng nhập hoặc mật khẩu thành viên không đúng");
           }
         },
         error: function(error) {
-          // Xử lý khi có lỗi xảy ra trong quá trình gửi yêu cầu
-          console.log("Đã xảy ra lỗi: " + error);
+          // Xử lý khi có lỗi xảy ra trong quá trình gửi yêu cầu đăng nhập thành viên
+          console.log("Đã xảy ra lỗi khi đăng nhập thành viên: " + error);
+        }
+      });
+  
+      // Gửi yêu cầu AJAX POST đến API đăng nhập cho quản lý
+      $.ajax({
+        url: "https://localhost:7206/api/Manager/login-Manager",
+        type: "POST",
+        contentType: "application/json",
+        data: JSON.stringify(managerData),
+        success: function(response) {
+          if (response.data) {
+            // Xử lý đăng nhập quản lý thành công
+            var manager = response.data;
+            window.location.href = "manager.html";
+            sessionStorage.setItem("loggedInManager", JSON.stringify(manager));
+          } else {
+            // Xử lý khi tên đăng nhập hoặc mật khẩu quản lý không đúng
+            console.log("Tên đăng nhập hoặc mật khẩu quản lý không đúng");
+          }
+        },
+        error: function(error) {
+          // Xử lý khi có lỗi xảy ra trong quá trình gửi yêu cầu đăng nhập quản lý
+          console.log("Đã xảy ra lỗi khi đăng nhập quản lý: " + error);
         }
       });
     });
   });
-*/
-                if (response.data) {
-                    // Kiểm tra dữ liệu phản hồi nếu thành công
-                    var member = response.data;
-                    if (member.role === "admin") {
-                        // Người dùng có vai trò là admin
-                        // Xử lý dữ liệu ở đây
-                        window.location.href = "admin.html";
-                    } else {
-                        // Người dùng không có vai trò là admin
-                        // Xử lý dữ liệu thành viên ở đây
-                        window.location.href = "index.html";
-                    }
-                    // Lưu session đăng nhập
-                    sessionStorage.setItem("loggedInMember", JSON.stringify(member));
-                } else {
-                    // Xử lý khi đăng nhập không thành công
-                    console.log("Tên đăng nhập hoặc mật khẩu không đúng");
-                }
-            },
-            error: function(error) {
-                // Xử lý khi có lỗi xảy ra trong quá trình gửi yêu cầu
-                console.log("Đã xảy ra lỗi: " + error);
-            }
-        });
-    });
-});
+  
