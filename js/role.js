16ad860d49441b58b225aa794fec8415172bb7e3
khai14tv <khai14tv@gmail.com>
2023-06-27 10:51:43 +0700

    add admin page

--- js/role.js
+++ js/role.js
@@ -1,7 +1,7 @@
 //Show trang manager khi login account role 
 
-const managerlink = document.getElementById("manager-link");
-if (currentUser.role === "admin") {
+const rolelink = document.getElementById("manager-link");
+if (currentUser.role === "manager") {
     managerlink.style.display = "block";
 } else {
     managerlink.style.display = "none";
