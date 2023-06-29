2ffd66adab89714376f7fc7a3be07d6b7ff0fc6d
MTT18 <lammjnhphong4560@gmail.com>
2023-06-29 10:39:57 +0700

    update code

--- js/list-index.js
+++ js/list-index.js
@@ -1,43 +1,54 @@
 
-$(document).ready(function(){
+$(document).ready(function () {
     var loggedInMember = sessionStorage.getItem("loggedInMember");
-    if(loggedInMember){
+    if (loggedInMember) {
         var mem = JSON.parse(loggedInMember);
 
         var memberFullName = mem.memberFullName;
         var memberImage = mem.memberImage;
-
         $('#fullname').html(memberFullName);
-        $('#member-image').attr('src', memberImage);
+        $('#image').attr('src', memberImage);
     }
 });
 
 
-$(document).ready(function(){
-    $("#logoutButton").click(function() {
+$(document).ready(function () {
+    $("#logoutButton").click(function () {
         // Clear the session storage
         sessionStorage.removeItem("loggedInMember");
         // Redirect to the login page or perform any other desired action
-        window.location.href = "sign-in.html";
+        window.location.href = "feed.html";
     });
 })
 
-$(document).ready(function(){
-    $.ajax({
-        url: 'đường_dẫn_đến_endpoint_api',
-        method: 'GET',
-        success: function(response) {
-          // Xử lý kết quả trả về ở đây
-        },
-        error: function(xhr, status, error) {
-          // Xử lý lỗi ở đây (nếu có)
-        }
-      });
-      
+// $(document).ready(function() {
+//     var loggedInMember = sessionStorage.getItem("loggedInMember");
+//     if (loggedInMember) {
+//       var mem = JSON.parse(loggedInMember);
+
+//       var memberId = mem.memberId;
+//     //   var postId = mem.postId;
+
+//       // Gửi AJAX request để lấy thông tin của member
+//       $.ajax({
+//         url: "https://localhost:7206/api/Member/ID-Member?id=Mem40080c3",
+//         type: "GET",
+//         data: { memberId: memberId },
+//         success: function(memberResponse) {
+//           // Xử lý và hiển thị thông tin của member
+//           var memberFullName = memberResponse.fullName;
+//           var memberImage = memberResponse.image;
+
+//           $('#fullname1').html(memberFullName);
+//           $('#member-image1').attr('src', memberImage);
+//         },
+//         error: function(xhr, status, error) {
+//           // Xử lý lỗi nếu có
+//         }
+//       });
+//     }
+//   });
 
 
 
-})
 
-
-  
