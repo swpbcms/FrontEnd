d44dd62baafae5319f176832400a1fbe834435e4
nghiant02 <nghia2852002@gmail.com>
2023-06-30 11:42:10 +0700

    update v2

--- js/id-member.js
+++
@@ -1,8 +0,0 @@
-// Fetch member ID from API and update the HTML element
-fetch('https://localhost:7206/api/Member/ID-Member?id=Mem047047c')
-.then(response => response.json())
-.then(data => {
-  const memberId = data.memberId;
-  document.getElementById('memberIdInput').value = memberId;
-})
-.catch(error => console.log('Error:', error));
--- js/update.js
+++ js/update.js
@@ -1,45 +1,58 @@
 $(document).ready(function() {
-    // Xử lý sự kiện submit của biểu mẫu
-    $("#signup").submit(function(event) {
-      event.preventDefault(); // Ngăn chặn gửi yêu cầu mặc định
+
+  // Lấy memberId từ sessionStorage
+  var memid;
+  var loggedInMember = sessionStorage.getItem("loggedInMember");
+    if (loggedInMember) {
+        var mem = JSON.parse(loggedInMember);
+
+        memid = mem.memberId;
+        $('#memberIdInput').html(memid);
+
+    }
   
-      // Lấy giá trị tên đăng nhập và mật khẩu từ người dùng
-      // Lấy giá trị từ các trường nhập liệu
-      var memberId = $("#memberIdInput").val();
-      var gender = $("input[name='gender']:checked").val();
-      var image = ""; // Set the member image value here
-      var fullName = $("#memberFullNameInput").val();
-      var email = $("#memberEmailInput").val();
-      var password = $("#memberPasswordInput").val();
-      var dob = $("#memberDateOfBirthInput").val();
-  
-      // Tạo đối tượng chứa dữ liệu gửi đi
-      var data = {
-        memberId: memberId,
-        memberGender: gender,
-        memberImage: image,
-        memberFullName: fullName,
-        memberEmail: email,
-        memberPassword: password,
-        memberDob: dob
-      };
-  
-      // Gửi yêu cầu AJAX PUT đến API
-      $.ajax({
-        url: "https://localhost:7206/api/Member/Update-member",
-        type: "PUT",
-        contentType: "application/json-patch+json",
-        data: JSON.stringify(data),
-        success: function(response) {
-          // Xử lý phản hồi từ API
-          console.log("Cập nhật thông tin thành viên thành công");
-          // Thực hiện các hành động khác sau khi cập nhật thông tin thành viên thành công
-        },
-        error: function(error) {
-          // Xử lý khi có lỗi xảy ra trong quá trình gửi yêu cầu
-          console.log("Đã xảy ra lỗi khi cập nhật thông tin thành viên: " + error);
+  // Xử lý sự kiện submit của biểu mẫu
+  $("#signup").submit(function(event) {
+    event.preventDefault(); // Ngăn chặn gửi yêu cầu mặc định
+
+    // Lấy giá trị từ các trường nhập liệu
+    var id = $("#memberIdInput").val();
+    var gender = $("input[name='genderupdate']:checked").val();
+    var image = ""; // Set the member image value here
+    var fullName = $("#FullNameInput").val();
+    var email = $("#EmailInput").val();
+    var password = $("#PasswordInput").val();
+    var dob = $("#DateOfBirthInput").val();
+
+    // Tạo đối tượng chứa dữ liệu gửi đi
+    var data = {
+      memberId: id,
+      memberGender: gender,
+      memberImage: image,
+      memberFullName: fullName,
+      memberEmail: email,
+      memberPassword: password,
+      memberDob: dob
+    };
+
+    // Gửi yêu cầu AJAX PUT đến API
+    $.ajax({
+      url: "https://localhost:7206/api/Member/Update-member",
+      type: "PUT",
+      contentType: "application/json",
+      data: JSON.stringify(data),
+      success: function(response) {
+        // Xử lý phản hồi từ API
+        if (response.data) {
+        } else {
+          // Xử lý khi đăng ký không thành công
+          console.log("thay đổi không thành công");
         }
-      });
+      },
+      error: function(error) {
+        // Xử lý khi có lỗi xảy ra trong quá trình gửi yêu cầu
+        console.log("Đã xảy ra lỗi khi cập nhật thông tin thành viên: " + error);
+      }
     });
   });
-  
+});
