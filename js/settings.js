e2977f8a974be59637da210db044403d0adecba6
nghiant02 <nghia2852002@gmail.com>
2023-07-06 13:06:09 +0700

    delete member (done) & fix update member (v2)

--- js/settings.js
+++ js/settings.js
@@ -57,3 +57,68 @@ $(document).ready(function() {
     }
   });
 });
+
+
+$(document).ready(function() {
+  // Xử lý sự kiện click nút xóa tài khoản
+  $("#deleteAccountButton").click(function(event) {
+    event.preventDefault(); // Ngăn chặn hành vi mặc định của nút submit
+
+    // Hiển thị thông báo xác nhận xóa tài khoản
+    Swal.fire({
+      title: "Xóa tài khoản",
+      text: "Bạn có chắc chắn muốn xóa tài khoản?",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonText: "Xóa",
+      cancelButtonText: "Hủy"
+    }).then(function(result) {
+      if (result.isConfirmed) {
+        // Lấy ID từ session
+        var loggedInMember = sessionStorage.getItem("loggedInMember");
+        if (loggedInMember) {
+          var mem = JSON.parse(loggedInMember);
+          var memberId = mem.memberId;
+
+          // Gửi yêu cầu DELETE đến API
+          deleteAccount(memberId);
+        } else {
+          console.log("Người dùng chưa đăng nhập");
+        }
+      }
+    });
+  });
+
+  // Hàm xóa tài khoản
+  function deleteAccount(memberId) {
+    // Gửi yêu cầu DELETE đến API
+    $.ajax({
+      url: "https://localhost:7206/api/Member/Delete-member?id=" + memberId,
+      type: "DELETE",
+      headers: {
+        "accept": "*/*"
+      },
+      success: function(response) {
+        // Xử lý phản hồi từ API
+        if (response.data) {
+          // Hiển thị thông báo xóa thành công (nếu cần)
+          Swal.fire({
+            icon: "success",
+            title: "Xóa tài khoản thành công",
+            showConfirmButton: false,
+            timer: 1500
+          }).then(function() {
+            // Chuyển hướng đến trang khác (nếu cần)
+            window.location.href = "feed.html";
+          });
+        } else {
+          console.log("Xóa tài khoản không thành công");
+        }
+      },
+      error: function(error) {
+        console.log("Đã xảy ra lỗi khi xóa tài khoản: " + error);
+        console.log(error);
+      }
+    });
+  }
+});
