5ea1da36c0f86d271c2669078ee684a0b4b41a91
Nghĩa Nguyễn Trọng <nghia2852002@gmail.com>
2023-08-04 13:04:43 +0700

    update memberStatus

--- js/register.js
+++ js/register.js
@@ -47,93 +47,43 @@ $(document).ready(function() {
       return; // Stop form submission
     }
 
-// ... (previous code remains the same)
+    // Validate avatar upload
+    var avatarInput = $("#avatarInput")[0];
+    var avatarFile = avatarInput.files[0]; // Get the first selected file
+    var image = ""; // Set the member image value here
 
-// Validate avatar upload
-var avatarInput = $("#avatarInput")[0];
-var avatarFile = avatarInput.files[0]; // Get the first selected file
-var image = ""; // Set the member image value here
+    if (!avatarFile) {
+      // Show validation error using Swal.fire
+      Swal.fire({
+        icon: "error",
+        title: "Validation Error",
+        text: "Please select an avatar image.",
+      });
+      return; // Stop form submission
+    }
 
-if (!avatarFile) {
-  // Show validation error using Swal.fire
-  Swal.fire({
-    icon: "error",
-    title: "Validation Error",
-    text: "Please select an avatar image.",
-  });
-  return; // Stop form submission
-}
+    // Read the selected image file using FileReader API
+    var reader = new FileReader();
 
-// Read the selected image file using FileReader API
-var reader = new FileReader();
+    reader.onload = function(event) {
+      // Base64-encoded image data
+      image = event.target.result;
 
-reader.onload = function(event) {
-  // Base64-encoded image data
-  image = event.target.result;
+      // Continue with form submission after reading the image
+      submitForm(username, password, confirmPassword, gender, image, fullName, email, dob);
+    };
 
-  // Continue with form submission after reading the image
-  submitForm(username, password, confirmPassword, gender, image, fullName, email, dob);
-};
+    reader.onerror = function() {
+      // Show error if image reading fails
+      Swal.fire({
+        icon: "error",
+        title: "Error",
+        text: "Failed to read the selected image file.",
+      });
+    };
 
-reader.onerror = function() {
-  // Show error if image reading fails
-  Swal.fire({
-    icon: "error",
-    title: "Error",
-    text: "Failed to read the selected image file.",
-  });
-};
-
-// Read the image file as data URL (Base64)
-reader.readAsDataURL(avatarFile);
-
-    // Check the existence of username and email in the API
-    $.ajax({
-      url: "https://localhost:7206/api/Member/All-Member",
-      type: "GET",
-      contentType: "application/json",
-      success: function(response) {
-        // Convert the response data to an array of usernames and emails
-        var usernames = response.data.map(function(member) {
-          return member.memberUserName;
-        });
-        var emails = response.data.map(function(member) {
-          return member.memberEmail;
-        });
-
-        // Check if the username or email already exists in the API
-        if (usernames.includes(username)) {
-          // Show validation error using Swal.fire
-          Swal.fire({
-            icon: "error",
-            title: "Validation Error",
-            text: "Username already exists.",
-          });
-          return; // Stop form submission
-        }
-
-        if (emails.includes(email)) {
-          // Show validation error using Swal.fire
-          Swal.fire({
-            icon: "error",
-            title: "Validation Error",
-            text: "Email already exists.",
-          });
-          return; // Stop form submission
-        }
-
-        // If all validations pass, proceed with form submission
-        submitForm(username, password, confirmPassword, gender, fullName, email, dob);
-      },
-      error: function(error) {
-        // Show error if API request fails
-        Swal.fire({
-          icon: "error",
-          title: "Error",
-          text: "Failed to check username and email existence in the API.",
-        });
-      },
-    });
+    // Read the image file as data URL (Base64)
+    reader.readAsDataURL(avatarFile);
   });
 });
 
@@ -159,44 +109,88 @@ function submitForm(username, password, confirmPassword, gender, image, fullName
     return; // Stop form submission
   }
 
-  // Tạo đối tượng chứa dữ liệu gửi đi
-  var data = {
-    memberUserName: username,
-    memberPassword: password,
-    memberGender: gender,
-    memberImage: image,
-    memberFullName: fullName,
-    memberEmail: email,
-    memberDob: dob,
-    memberStatus: "pending", // New field to indicate pending approval
+  // Validate username and email existence
+  validateUsernameAndEmail(username, email, function(errors) {
+    if (Object.keys(errors).length > 0) {
+      // Show validation errors using Swal.fire
+      Swal.fire({
+        icon: "error",
+        title: "Validation Error",
+        html: Object.values(errors).join("<br>"),
+      });
+    } else {
+      // Proceed with form submission
 
-  };
+      // Tạo đối tượng chứa dữ liệu gửi đi
+      var data = {
+        memberUserName: username,
+        memberPassword: password,
+        memberGender: gender,
+        memberImage: image,
+        memberFullName: fullName,
+        memberEmail: email,
+        memberDob: dob,
+        memberStatus: "pending", // New field to indicate pending approval
+      };
 
-  // Gửi yêu cầu AJAX POST đến API
+      // Gửi yêu cầu AJAX POST đến API
+      $.ajax({
+        url: "https://localhost:7206/api/Member/Register-Member",
+        type: "POST",
+        contentType: "application/json",
+        data: JSON.stringify(data),
+        success: function(response) {
+          // Xử lý phản hồi từ API
+          if (response.data) {
+            // Xử lý khi đăng ký thành công
+            Swal.fire({
+              icon: "success",
+              title: "Success",
+              text: "Đăng ký thành công. Vui lòng chờ quản lý duyệt tài khoản.",
+            }).then(() => {
+              window.location.href = "sign-in.html"; // Chuyển đến trang đăng nhập sau khi đăng ký thành công
+            });
+          } else {
+            // Xử lý khi đăng ký không thành công
+            Swal.fire({
+              icon: "error",
+              title: "Error",
+              text: "Đăng ký không thành công",
+            });
+          }
+        },
+      });
+    }
+  });
+}
+
+function validateUsernameAndEmail(username, email, callback) {
   $.ajax({
-    url: "https://localhost:7206/api/Member/Register-Member",
-    type: "POST",
+    url: "https://localhost:7206/api/Member/All-Member",
+    type: "GET",
     contentType: "application/json",
-    data: JSON.stringify(data),
     success: function(response) {
-      // Xử lý phản hồi từ API
-      if (response.data) {
-        // Xử lý khi đăng ký thành công
-        Swal.fire({
-          icon: "success",
-          title: "Success",
-          text: "Đăng ký thành công. Vui lòng chờ quản lý duyệt tài khoản.",
-        }).then(() => {
-          window.location.href = "sign-in.html"; // Chuyển đến trang đăng nhập sau khi đăng ký thành công
-        });
-      } else {
-        // Xử lý khi đăng ký không thành công
-        Swal.fire({
-          icon: "error",
-          title: "Error",
-          text: "Đăng ký không thành công",
-        });
+      var usernames = response.data.map(function(member) {
+        return member.memberUserName;
+      });
+      var emails = response.data.map(function(member) {
+        return member.memberEmail;
+      });
+
+      var errors = {};
+
+      if (usernames.includes(username)) {
+        errors.username = "Username already exists.";
       }
+
+      if (emails.includes(email)) {
+        errors.email = "Email already exists.";
+      }
+
+      callback(errors);
+    },
+    error: function(error) {
+      callback({ apiError: "Failed to check username and email existence in the API." });
     },
   });
 }
